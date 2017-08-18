ebf7f4c01f1fb85a70f1a4f52c35061eabcf2eb1
Prasath Rao <prasath@ajirasoft.com>
2017-08-18 13:35:01 +0530

    Hide window on interupt

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 var electron = require('electron')
+var {app, BrowserWindow} = electron;
 var system = require('@paulcbetts/system-idle-time/addon');
-var app = electron.app
-var BrowserWindow = electron.BrowserWindow
 var checkIdleTime;
-const ipc = require('electron').ipcRenderer
+var mainWindow;
 
-app.on('ready', function(){
-  var waitingTime = 10000
+app.on('ready', function() {
+  setTimer();
+});
+
+function setTimer() {
+  var waitingTime = 10000;
   checkIdleTime = setInterval(function () {
     if(system.getIdleTime() > waitingTime) {
       createWindow()
     }
   }, 1000);
-
-})
+}
 
 function createWindow() {
-  var mainWindow = new BrowserWindow()
-  mainWindow.setFullScreen(true)
-  mainWindow.loadURL('file://' + __dirname + '/promotion.html')
+  if(mainWindow == null) {
+    mainWindow = new BrowserWindow()
+    mainWindow.setFullScreen(true)
+    mainWindow.loadURL('file://' + __dirname + '/promotion.html')
+  } else {
+    app.show();
+    app.focus();
+  }
   clearInterval(checkIdleTime);
 }
+
+function hideScreen() {
+  app.hide();
+  setTimer();
+}
+
+exports.hideScreen = hideScreen;
+
+var ipc = electron.ipcMain;
+
+ipc.on('errorInWindow', function(event, data){
+    console.log(data)
+});
